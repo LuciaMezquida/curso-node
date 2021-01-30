3a6f784df0a574559d0d1742406cc85e3024d053
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-01-30 12:56:34 +0100

    Create a post request

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({​​​​​extended:true}​​​​​)); 
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log("El servidor corre en el puerto 3000");
