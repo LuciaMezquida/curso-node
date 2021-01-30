49af1b244de8e45a27075c40a42006bc12886cab
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-01-30 12:06:08 +0100

    Add server with express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const app = require("express");
+//importo la libreria de express y la meto en una constante
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("El servidor corre en el puerto 3000-------------");
+});
+
+app.get("/", (req, res) => {
+  res.send;
+});
