94d5c9d55e8e3e5cea88f98b7cbae45803e2a16e
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-01-30 12:51:22 +0100

    Import body-parser library

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 //importo la libreria de express y la meto en una constante
 const express = require("express");
+//libreria que recoge los datos
+const bodyParser = require("body-parser");
 const app = express();
+
 const port = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({​​​​​extended:true}​​​​​)); 
+
+
 app.listen(port, () => {
-  console.log("El servidor corre en el puerto 3000-------------");
+  console.log("El servidor corre en el puerto 3000");
+});
+//localhost:3000 + en el primer parametro == localhost:3000/
+app.get("/", (req, res) => {
+  res.send("Salve Muka");
+});
+//localhost:3000/muka
+app.get("/muka", (req, res) => {
+  res.send("Salve Muka, dueña del mundo");
 });
 
-app.get("/", (req, res) => {
-  res.send;
+//localhost:3000/enviar
+app.post("/enviar", (req, res) => {
+  console.log("Esta es la ruta post");
+  let data = req.body;
+  console.log(data);
+  res.send("Enviado");
 });
